0b84ee951554e5585c4e831e21bbfea94e6d01e4
KangerDrew <kangdrew95@gmail.com>
2021-10-21 18:42:03 -0700

    Modified the test file for TrucksController

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,2 +1,13 @@
 class TrucksController < ApplicationController
+
+  def index
+    @trucks = Trucks.all.order(created_at: :desc)
+  end
+
+  def show
+    @truck = Truck.find params[:id]
+  end
+
+
+
 end
--- test/controllers/trucks_controller_test.rb
+++ test/controllers/trucks_controller_test.rb
@@ -4,4 +4,10 @@ class TrucksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get trucks_index_url
+    assert_response :success
+  end
+
 end
